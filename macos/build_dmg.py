01fcebd972878bc9116b0bed46bdb0f425b9e7e4
Just van Rossum <justvanrossum@gmail.com>
2025-12-02 10:29:38 +0100

    Try to use mkstemp instead of mktemp

--- macos/build_dmg.py
+++ macos/build_dmg.py
@@ -20,7 +20,7 @@ with tempfile.TemporaryDirectory() as imgPath:
     try:
         os.symlink("/Applications", os.path.join(imgPath, "Applications"))
 
-        tmpImagePath = tempfile.mktemp(suffix=".dmg")
+        tmpImagePath = tempfile.mkstemp(suffix=".dmg")
         try:
             createCommand = [
                 "hdiutil",
